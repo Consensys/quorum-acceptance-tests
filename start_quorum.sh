8f5e3ad9934852c263dd3306afd3e2dec69c00a6
Trung Nguyen <trung.n.k@gmail.com>
2018-09-22 11:22:31 -0400

    travis integration

--- start_quorum.sh
+++ start_quorum.sh
@@ -21,7 +21,7 @@ popd
 
 pushd quorum-cloud/aws/templates
 ${TERRAFORM_CMD} init -no-color -backend-config=terraform.auto.backend_config
-TF_LOG=debug ${TERRAFORM_CMD} apply -var consensus_mechanism=${CONSENSUS} -auto-approve
+${TERRAFORM_CMD} apply -var consensus_mechanism=${CONSENSUS} -auto-approve
 popd
 
 echo "Wait for the Quorum Network being ready"
