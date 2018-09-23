a0a39c7eeee49c2b6f74f3d900f6088d0be0fa1b
Trung Nguyen <trung.n.k@gmail.com>
2018-09-23 00:22:40 -0400

    travis integration

--- start_quorum.sh
+++ start_quorum.sh
@@ -6,13 +6,14 @@ TERRAFORM_CMD=${TERRAFORM_CMD:-terraform}
 
 echo "Provisioning Quorum Network"
 pushd ${QUORUM_CLOUD_TEMPLATES_DIR}/_terraform_init > /dev/null
-${TERRAFORM_CMD} init
-${TERRAFORM_CMD} apply -var network_name=ci-${TF_VAR_consensus_mechanism}-${TRAVIS_COMMIT::6} -auto-approve
+${TERRAFORM_CMD} init > /dev/null
+${TERRAFORM_CMD} apply -var network_name=ci-${TF_VAR_consensus_mechanism}-${TRAVIS_COMMIT::6} -auto-approve > /dev/null
 popd > /dev/null
 
 pushd ${QUORUM_CLOUD_TEMPLATES_DIR} > /dev/null
-${TERRAFORM_CMD} init -backend-config=terraform.auto.backend_config
-${TERRAFORM_CMD} apply -auto-approve
+${TERRAFORM_CMD} init -backend-config=terraform.auto.backend_config > /dev/null
+${TERRAFORM_CMD} apply -auto-approve > /dev/null
+${TERRAFORM_CMD} output
 export private_key_file=$(${TERRAFORM_CMD} output -json | jq -r .private_key_file.value)
 export bastion_host_ip=$(${TERRAFORM_CMD} output -json | jq -r .bastion_host_ip.value)
 chmod 600 ${private_key_file}
@@ -21,14 +22,14 @@ popd > /dev/null
 echo "Wait for the Quorum Network being ready"
 f="config/application-local.yml"
 count=1
-while [ "$count" -le 40 ] && [ ! -f ${f} ]; do
+while [ "$count" -le 100 ] && [ ! -f ${f} ]; do
     sleep 3
     scp -o ConnectTimeout=10 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet \
         -i ${private_key_file} ec2-user@${bastion_host_ip}:/qdata/quorum_metadata ${f} || echo "Reading metadata...${count}"
     count=$((count+1))
 done
 
-if [ -f ${f} ]; then
+if [ ! -f ${f} ]; then
     echo "Timed out!"
     exit 1
 fi
