34b3d95e02dff37cb9a121476281581724f4a3e9
Trung Nguyen <trung.n.k@gmail.com>
2018-09-22 23:43:30 -0400

    travis integration

--- start_quorum.sh
+++ start_quorum.sh
@@ -11,20 +11,27 @@ ${TERRAFORM_CMD} apply -var network_name=ci-${TF_VAR_consensus_mechanism}-${TRAV
 popd > /dev/null
 
 pushd ${QUORUM_CLOUD_TEMPLATES_DIR} > /dev/null
-${TERRAFORM_CMD} init -no-color -backend-config=terraform.auto.backend_config
+${TERRAFORM_CMD} init -backend-config=terraform.auto.backend_config
 ${TERRAFORM_CMD} apply -auto-approve
 export private_key_file=$(${TERRAFORM_CMD} output -json | jq .private_key_file.value)
-echo ${private_key_file}
 export bastion_host_ip=$(${TERRAFORM_CMD} output -json | jq .bastion_host_ip.value)
-echo ${bastion_host_ip}
+chmod 600 ${private_key_file}
 popd > /dev/null
 
 echo "Wait for the Quorum Network being ready"
-while [ ! -f "config/application-local.yml" ]; do
+f="config/application-local.yml"
+count=1
+while [ "$count" -le 40 ] && [ ! -f ${f} ]; do
     sleep 3
     scp -o ServerAliveInterval=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet \
-        -i ${private_key_file} ec2-user@${bastion_host_ip}:/qdata/quorum_metadata config/application-local.yml || echo "Reading metadata..."
+        -i ${private_key_file} ec2-user@${bastion_host_ip}:/qdata/quorum_metadata ${f} || echo "Reading metadata...${count}"
+    count=$((count+1))
 done
+
+if [ -f ${f} ]; then
+    echo "Timed out!"
+    exit 1
+fi
 
 echo "Start SOCKS proxy for SSH tunnelling"
 ssh -D 5000 -N -o ServerAliveInterval=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=quiet \
