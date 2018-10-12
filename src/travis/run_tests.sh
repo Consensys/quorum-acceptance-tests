db25757683cd5d241dd8bb66f09af9262c33bbfe
Trung Nguyen <trung.n.k@gmail.com>
2018-10-12 12:59:57 -0400

    show travis build number in the report

--- src/travis/run_tests.sh
+++ src/travis/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Running tests"
+ENV_DIR="travis-build-${TRAVIS_JOB_NUMBER}"
+mkdir -p env/${ENV_DIR}
 mvn clean test -q \
   -DTRAVIS=${TRAVIS} \
-  -DCONSENSUS=${TF_VAR_consensus_mechanism}
+  -DCONSENSUS=${TF_VAR_consensus_mechanism} \
+  -Denv=${ENV_DIR}
