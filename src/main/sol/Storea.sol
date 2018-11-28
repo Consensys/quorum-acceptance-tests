95873112e08bce12f6cea17eac9d5dbee44d784a
Trung Nguyen <trung.n.k@gmail.com>
2018-11-28 11:12:10 -0500

    reformat sol code

--- src/main/sol/Storea.sol
+++ src/main/sol/Storea.sol
@@ -1,38 +1,46 @@
 pragma solidity ^0.4.21;
+
 contract storeb {
-  function setb(uint x) public;
-  function setc(uint x) public;
-  function getb() public view returns (uint);
-  function getc() public view returns (uint);
+    function setb(uint x) public;
+
+    function setc(uint x) public;
+
+    function getb() public view returns (uint);
+
+    function getc() public view returns (uint);
 }
+
 contract storea {
-  uint public a;
-  storeb b;
-  constructor (uint initVal, address _addrb) public {
-    a = initVal;
-    b = storeb(_addrb);
-  }
+    uint public a;
+    storeb b;
 
-  function geta() public view returns (uint retVal) {
-    return a;
-  }
-  function getb() public view returns (uint retVal) {
-    return b.getb();
-  }
-  function getc() public view returns (uint retVal) {
-    return b.getc();
-  }
+    constructor (uint initVal, address _addrb) public {
+        a = initVal;
+        b = storeb(_addrb);
+    }
 
-  function seta(uint x) public {
-      uint mc = b.getb();
-      a = x * mc;
-  }
+    function geta() public view returns (uint retVal) {
+        return a;
+    }
 
-  function setb(uint x) public {
-    b.setb(x);
-  }
+    function getb() public view returns (uint retVal) {
+        return b.getb();
+    }
 
-  function setc(uint x) public {
-    b.setc(x);
-  }
+    function getc() public view returns (uint retVal) {
+        return b.getc();
+    }
+
+    function seta(uint x) public {
+        uint mc = b.getb();
+        a = x * mc;
+    }
+
+    function setb(uint x) public {
+        b.setb(x);
+    }
+
+    function setc(uint x) public {
+        b.setc(x);
+    }
 }
--- src/main/sol/Storec.sol
+++ src/main/sol/Storec.sol
@@ -1,13 +1,17 @@
 pragma solidity ^0.4.21;
+
 contract storec {
-  uint public c;
-  constructor (uint pval) public {
-    c = pval;
-  }
-  function setc(uint x) public {
-    c = x;
-  }
-  function getc() public view returns (uint retVal) {
-    return c;
-  }
+    uint public c;
+
+    constructor (uint pval) public {
+        c = pval;
+    }
+
+    function setc(uint x) public {
+        c = x;
+    }
+
+    function getc() public view returns (uint retVal) {
+        return c;
+    }
 }
