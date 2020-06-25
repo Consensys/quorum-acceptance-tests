df8528bd3b92fa27ab8db704fe58aa526f4a582b
amalraj.manigmail.com <amalraj.manigmail.com>
2020-06-25 14:31:38 +0800

    change quorum image for testing

--- networks/template/variables.tf
+++ networks/template/variables.tf
@@ -34,7 +34,7 @@ variable "exclude_initial_nodes" {
 
 variable "quorum_docker_image" {
   type        = object({ name = string, local = bool })
-  default     = { name = "quorumengineering/quorum:latest", local = false }
+  default     = { name = "amalrajmani/quorum:perm1", local = false }
   description = "Local=true indicates that the image is already available locally and don't need to pull from registry"
 }
 
