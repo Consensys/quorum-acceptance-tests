0bd4741a0914a0a2a49eead2037c7d1acb5e7912
amalraj mani <amalraj.manigmail.com>
2020-08-31 22:04:06 +0800

    split solc java files' binary string to make compilation pass if the binary string is too long

---
+++ scripts/modify-source.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+srcFilePath=$1
+echo "process java source files from $srcFilePath ..."
+cd $srcFilePath
+for file in `ls -1 ./*.java` ; do
+    if [ -f $file ]; then
+        echo "Processing $file"
+        mv "$file" "$file.bk"
+        awk '{ if( match($0, /^(.*?String BINARY = )"([^"]+)"/, m))
+            {
+                print m[1], "new StringBuilder()"
+                n = split (m[2], ary, /.{1,200}/, seps);
+                for (i=1; i<=n; i++)
+                   printf ".append(\"%s\")%s", seps[i], (i<n ? "\n" : ".toString();\n")
+           } else {
+                print $0
+           }
+        }' "$file.bk" > "$file"
+        rm "$file.bk"
+    fi
+done
