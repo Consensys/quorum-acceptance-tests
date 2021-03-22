e6767798e22d65d8306f2ff2f493fe7bde3c0768
Ricardo Silva <ricardo.silva@consensys.net>
2021-03-22 17:41:27 +0000

    feature: created new ignite configuration for Besu/Tessera/EthSigner

---
+++ networks/_modules/ignite-besu/bootstrap.tf
@@ -0,0 +1,200 @@
+locals {
+  enode_urls = formatlist("\"enode://%s@%s:%d\"", quorum_bootstrap_node_key.nodekeys-generator[*].hex_node_id, var.besu_networking[*].ip.private)
+
+  # metadata for network subjected to initial participants input
+  network = {
+    hexNodeIds      = [for idx in local.node_indices : quorum_bootstrap_node_key.nodekeys-generator[idx].hex_node_id if lookup(local.node_initial_paticipants, idx, "false") == "true"]
+    networking = [for idx in local.node_indices : var.besu_networking[idx] if lookup(local.node_initial_paticipants, idx, "false") == "true"]
+    enode_urls      = [for idx in local.node_indices : local.enode_urls[idx] if lookup(local.node_initial_paticipants, idx, "false") == "true"]
+  }
+
+  chainId = random_integer.network_id.result
+}
+
+data "null_data_source" "meta" {
+  count = local.number_of_nodes
+  inputs = {
+    idx             = count.index
+    tmKeys          = join(",", [for k in local.tm_named_keys_alloc[count.index] : element(quorum_transaction_manager_keypair.tm.*.key_data, index(local.tm_named_keys_all, k))])
+    nodeUrl         = format("http://%s:%d", var.ethsigner_networking[count.index].ip.public, var.ethsigner_networking[count.index].port.http.external)
+    tmThirdpartyUrl = format("http://%s:%d", var.tm_networking[count.index].ip.public, var.tm_networking[count.index].port.thirdparty.external)
+    graphqlUrl      = format("http://%s:%d/graphql", var.besu_networking[count.index].ip.public, var.besu_networking[count.index].port.graphql.external)
+  }
+}
+
+resource "random_integer" "network_id" {
+  max = 3000
+  min = 1400
+}
+
+resource "quorum_bootstrap_network" "this" {
+  name       = local.network_name
+  target_dir = local.generated_dir
+}
+
+resource "quorum_bootstrap_keystore" "accountkeys-generator" {
+  count                = local.number_of_nodes
+  keystore_dir         = format("%s/%s%s/keystore", quorum_bootstrap_network.this.network_dir_abs, local.node_dir_prefix, count.index)
+  use_light_weight_kdf = true
+
+  dynamic "account" {
+    for_each = lookup(local.named_accounts_alloc, count.index)
+    content {
+      passphrase = ""
+      balance    = "1000000000000000000000000000"
+    }
+  }
+}
+
+resource "quorum_bootstrap_node_key" "nodekeys-generator" {
+  count = local.number_of_nodes
+}
+
+resource "quorum_transaction_manager_keypair" "tm" {
+  count = length(local.tm_named_keys_all)
+  config {
+    memory = 100000
+  }
+}
+
+resource "local_file" "tm" {
+  count    = length(local.tm_named_keys_all)
+  filename = format("%s/%s", local.tmkeys_generated_dir, element(local.tm_named_keys_all, count.index))
+  content  = quorum_transaction_manager_keypair.tm[count.index].key_data
+}
+
+data "quorum_bootstrap_genesis_mixhash" "this" {
+
+}
+
+resource "quorum_bootstrap_istanbul_extradata" "this" {
+  istanbul_addresses = [for idx in local.node_indices : quorum_bootstrap_node_key.nodekeys-generator[idx].istanbul_address if lookup(local.istanbul_validators, idx, "false") == "true"]
+}
+
+resource "local_file" "genesis-file" {
+  filename = format("%s/genesis.json", quorum_bootstrap_network.this.network_dir_abs)
+  content  = <<-EOF
+{
+  "config" : {
+    "chainId" : ${local.chainId},
+    "constantinoplefixblock" : 0,
+    "ibft2" : {
+      "blockperiodseconds" : 1,
+      "epochlength" : 30000,
+      "requesttimeoutseconds" : 10
+    },
+    "isQuorum": true
+  },
+  "nonce" : "0x0",
+  "timestamp": "0x00",
+  "gasLimit" : "0xFFFFFF00",
+  "difficulty" : "0x1",
+  "mixhash": "${data.quorum_bootstrap_genesis_mixhash.this.istanbul}",
+  "extraData": "${quorum_bootstrap_istanbul_extradata.this.extradata}",
+  "coinbase" : "0x0000000000000000000000000000000000000000",
+  "alloc": {
+      ${join(",", formatlist("\"%s\" : { \"balance\": \"%s\" }", quorum_bootstrap_keystore.accountkeys-generator[*].account[0].address, quorum_bootstrap_keystore.accountkeys-generator[*].account[0].balance))}
+    },
+}
+EOF
+}
+
+resource "local_file" "nodekey-file" {
+    filename = format("%s/nodekeys-tmp.json", quorum_bootstrap_network.this.network_dir_abs)
+    content  = <<-EOF
+    {
+    ${join(",", quorum_bootstrap_node_key.nodekeys-generator[*].hex_node_id)}
+}
+EOF
+}
+# TODO ricardolyn: we should create the dirs but nothing else
+resource "quorum_bootstrap_data_dir" "datadirs-generator" {
+  count    = local.number_of_nodes
+  data_dir = format("%s/%s%s", quorum_bootstrap_network.this.network_dir_abs, local.node_dir_prefix, count.index)
+  genesis  = local_file.genesis-file.content
+}
+
+resource "local_file" "static-nodes" {
+  count    = local.number_of_nodes
+  filename = format("%s/static-nodes.json", quorum_bootstrap_data_dir.datadirs-generator[count.index].data_dir_abs)
+  content  = "[${join(",", local.network.enode_urls)}]"
+}
+
+resource "local_file" "permissioned-nodes" {
+  count    = local.number_of_nodes
+  filename = format("%s/permissioned-nodes.json", quorum_bootstrap_data_dir.datadirs-generator[count.index].data_dir_abs)
+  content  = local_file.static-nodes[count.index].content
+}
+
+resource "local_file" "passwords" {
+  count    = local.number_of_nodes
+  filename = format("%s/%s", quorum_bootstrap_data_dir.datadirs-generator[count.index].data_dir_abs, local.password_file)
+  content  = ""
+}
+
+resource "local_file" "genesisfile" {
+    count    = local.number_of_nodes
+    filename = format("%s/%s", quorum_bootstrap_data_dir.datadirs-generator[count.index].data_dir_abs, local.genesis_file)
+    content  = quorum_bootstrap_data_dir.datadirs-generator[count.index].genesis
+}
+
+resource "local_file" "tmconfigs-generator" {
+  count    = local.number_of_nodes
+  filename = format("%s/%s%s/config.json", quorum_bootstrap_network.this.network_dir_abs, local.tm_dir_prefix, count.index)
+  content  = <<-JSON
+{
+    "useWhiteList": false,
+    "jdbc": {
+        "username": "sa",
+        "password": "",
+        "url": "[TO BE OVERRIDREN FROM COMMAND LINE]",
+        "autoCreateTables": true
+    },
+    "serverConfigs":[
+        {
+            "app":"ThirdParty",
+            "enabled": true,
+            "serverAddress": "http://${var.tm_networking[count.index].ip.private}:${var.tm_networking[count.index].port.thirdparty.internal}",
+            "communicationType" : "REST"
+        },
+        {
+            "app":"Q2T",
+            "enabled": true,
+            "serverAddress":"[TO BE OVERRIDDEN FROM COMMAND LINE]",
+            "communicationType" : "REST"
+        },
+        {
+            "app":"P2P",
+            "enabled": true,
+            "serverAddress":"http://${var.tm_networking[count.index].ip.private}:${var.tm_networking[count.index].port.p2p}",
+            "sslConfig": {
+              "tls": "OFF",
+              "generateKeyStoreIfNotExisted": true,
+              "serverKeyStore": "[TO BE OVERRIDREN FROM COMMAND LINE]",
+              "serverKeyStorePassword": "quorum",
+              "serverTrustStore": "[TO BE OVERRIDREN FROM COMMAND LINE]",
+              "serverTrustStorePassword": "quorum",
+              "serverTrustMode": "TOFU",
+              "knownClientsFile": "[TO BE OVERRIDREN FROM COMMAND LINE]",
+              "clientKeyStore": "[TO BE OVERRIDREN FROM COMMAND LINE]",
+              "clientKeyStorePassword": "quorum",
+              "clientTrustStore": "[TO BE OVERRIDREN FROM COMMAND LINE]",
+              "clientTrustStorePassword": "quorum",
+              "clientTrustMode": "TOFU",
+              "knownServersFile": "[TO BE OVERRIDREN FROM COMMAND LINE]"
+            },
+            "communicationType" : "REST"
+        }
+    ],
+    "peer": [${join(",", formatlist("{\"url\" : \"http://%s:%d\"}", var.tm_networking[*].ip.private, var.tm_networking[*].port.p2p))}],
+    "keys": {
+      "passwords": [],
+      "keyData": [${data.null_data_source.meta[count.index].inputs.tmKeys}]
+    },
+    "alwaysSendTo": [],
+    "features" : {
+      "enableRemoteKeyValidation" : "true"
+    }
+}
+JSON
+}
---
+++ networks/_modules/ignite-besu/variables.tf
@@ -0,0 +1,43 @@
+variable "network_name" {
+  default     = ""
+  description = "Name of the network being created. If empty, a random name will be used"
+}
+
+variable "tm_networking" {
+  type = list(object({
+    port = object({
+      thirdparty = object({ internal = number, external = number })
+      p2p        = number
+    })
+    ip = object({
+      private = string
+      public  = string
+    })
+  }))
+  description = "Networking configuration for `tessera` nodes in the network. Number of items must match `geth_networking`"
+}
+
+variable "besu_networking" {
+  type = list(object({
+    port = object({
+      http    = object({ internal = number, external = number })
+      ws      = object({ internal = number, external = number })
+      graphql = object({ internal = number, external = number })
+    })
+    ip = object({ private = string, public = string })
+  }))
+  description = "Networking configuration for `besu` nodes in the network. Number of items must match `tm_networking`"
+}
+
+variable "ethsigner_networking" {
+  type = list(object({
+    port  = object({ internal = number, external = number })
+    ip    = object({ private = string, public = string })
+  }))
+  description = "Networking configuration for `ethsigner` nodes in the network. Number of items must match `besu_networking`"
+}
+
+variable "output_dir" {
+  default     = "build"
+  description = "Target directory that contains generated resources for the network"
+}
