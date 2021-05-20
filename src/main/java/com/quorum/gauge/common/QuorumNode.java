c85533394e79b91d92298eccb283e6fd114af345
Nicolae Leonte <nicolae.leonte.go@gmail.com>
2021-05-20 11:27:25 +0100

    remove unnecessary changes to QuorumNode

--- src/main/java/com/quorum/gauge/common/QuorumNode.java
+++ src/main/java/com/quorum/gauge/common/QuorumNode.java
@@ -27,14 +27,5 @@ package com.quorum.gauge.common;
  */
 @Deprecated
 public enum QuorumNode {
-    Node1, Node1_PS1, Node1_PS2, Node1_PS3,
-    Node2, Node2_PS1, Node2_PS2, Node2_PS3,
-    Node3, Node3_PS1, Node3_PS2, Node3_PS3,
-    Node4, Node4_PS1, Node4_PS2, Node4_PS3,
-    Node5, Node5_PS1, Node5_PS2, Node5_PS3,
-    Node6, Node6_PS1, Node6_PS2, Node6_PS3,
-    Node7, Node7_PS1, Node7_PS2, Node7_PS3,
-    Node8, Node8_PS1, Node8_PS2, Node8_PS3,
-    Node9, Node9_PS1, Node9_PS2, Node9_PS3,
-    Node10, Node10_PS1, Node10_PS2, Node10_PS3,
+    Node1, Node2, Node3, Node4, Node5, Node6, Node7, Node8, Node9, Node10
 }
