fca9e5d14c193a745e3362de3b7dd7357119d5ef
Ricardo Silva <1945557+ricardolyn@users.noreply.github.com>
2021-09-03 14:57:57 +0100

    Update OpenJDK, Solidity, gauge & web3j version (#123)
    * config: upgrade versions for Java, gauge, web3j
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    sol: upgraded solidity version to 0.6.0
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: new changes required for Web3j
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: issues with constructor on store
    
    update: gauge version
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: byte array size needs to be 32 when sending the deposit _id
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    experiment: avoid issue with web3j
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    revert invalid changes
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: naming on Store contracts
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: set right version for solidity
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: set back original version
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    fix: remove unused version in docker file
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    disable temporary test upload
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    Use `latest` quorum image. to be reverted.
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    remove revert reason. to be reverted
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    Revert "remove revert reason. to be reverted"
    
    This reverts commit a073a98f219edd6ddd91191d2f0723bf45bd9a24.
    
    Revert "Use `latest` quorum image. to be reverted."
    
    This reverts commit 7c1b0940d58a462545267868845580662ae9a987.
    
    * tidy up
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    * Update web3j version
    
    * fix: use right version of web3j variable on the plugin definition
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    * ci/cd: only upload txt artifacts
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    * mvn: bump version of web3j to 4.8.7
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    * fix issues with contracts
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    * remove unused import
    
    Signed-off-by: Ricardo Silva <ricardo.silva@consensys.net>
    
    Co-authored-by: Antony Denyer <email@antonydenyer.co.uk>

--- src/main/sol/ContractCodeReader.sol
+++ src/main/sol/ContractCodeReader.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.17;
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.5.0;
 
 // this contract is to demonstrate ability to read other contract code/hash/size
 // via EXTCODECOPY, EXTCODEHASH and EXTCODESIZE opcodes
--- src/main/sol/permissions/v2/RoleManager.sol
+++ src/main/sol/permissions/v2/RoleManager.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.17;
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.5.0;
 
 import "./PermissionsUpgradable.sol";
 /** @title Role manager contract
@@ -129,11 +130,11 @@ contract RoleManager {
     /** @notice returns the role details for a passed role id and org
       * @param _roleId - unique identifier for the role being added
       * @param _orgId - org id to which the role belongs
-      * @return role id
-      * @return org id
-      * @return access type
-      * @return bool to indicate if the role is a voter role
-      * @return bool to indicate if the role is active
+      * @return roleId
+      * @return orgId
+      * @return accessType
+      * @return voter bool to indicate if the role is a voter role
+      * @return admin bool to indicate if the role is active
       */
     function getRoleDetails(string calldata _roleId, string calldata _orgId)
     external view returns (string memory roleId, string memory orgId,
@@ -149,11 +150,11 @@ contract RoleManager {
 
     /** @notice returns the role details for a passed role index
       * @param _rIndex - unique identifier for the role being added
-      * @return role id
-      * @return org id
-      * @return access type
-      * @return bool to indicate if the role is a voter role
-      * @return bool to indicate if the role is active
+      * @return roleId
+      * @return orgId
+      * @return accessType
+      * @return voter bool to indicate if the role is a voter role
+      * @return admin bool to indicate if the role is active
       */
     function getRoleDetailsFromIndex(uint256 _rIndex) external view returns
     (string memory roleId, string memory orgId, uint256 accessType,
